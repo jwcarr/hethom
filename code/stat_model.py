3c27c4f083f2bae7863d002477cd912430ac67f5
Jon Carr <4412666+jwcarr@users.noreply.github.com>
2023-04-18 10:12:43 +0100

    Update priors

--- code/stat_model.py
+++ code/stat_model.py
@@ -16,14 +16,14 @@ def paired_t_test(condition1, condition2):
 		trace = pm.sample(5000, tune=1000, chains=4, cores=4, return_inferencedata=True)
 	return trace
 
-def unpaired_t_test(condition1, condition2, params):
+def unpaired_t_test(condition1, condition2):
 	condition1 = np.array(condition1)
 	condition2 = np.array(condition2)
 	with pm.Model() as model:
-		μ1 = pm.Normal('μ1', mu=500, sigma=100)
+		μ1 = pm.Normal('μ1', mu=350, sigma=100)
 		σ1 = pm.Exponential('σ1', lam=0.01)
 		x1 = pm.Normal('x1', mu=μ1, sigma=σ1, observed=condition1)
-		μ2 = pm.Normal('μ2', mu=200, sigma=100)
+		μ2 = pm.Normal('μ2', mu=350, sigma=100)
 		σ2 = pm.Exponential('σ2', lam=0.01)
 		x2 = pm.Normal('x2', mu=μ2, sigma=σ2, observed=condition2)
 		Δ = pm.Deterministic('Δ', μ1 - μ2)
@@ -33,10 +33,10 @@ def unpaired_t_test(condition1, condition2, params):
 
 if __name__ == '__main__':
 
-	condition1 = generate_systems(10, 500, 50)
-	condition2 = generate_systems(10, 500, 50)
+	condition1 = generate_systems(10, 250, 50)
+	condition2 = generate_systems(10, 350, 50)
 
 	trace = unpaired_t_test(condition1, condition2)
 
-	az.plot_posterior(trace, hdi_prob=0.99)
+	az.plot_posterior(trace, hdi_prob=0.99, var_names=['μ1', 'μ2', 'Δ'])
 	az.utils.plt.show()
