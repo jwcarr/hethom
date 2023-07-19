8f8192a9d120fa8f0826acf31bc29f053524d693
Jon Carr <4412666+jwcarr@users.noreply.github.com>
2023-07-19 11:29:46 +0100

    Create new spoken word forms for Experiment 3

--- code/make_audio_labels.py
+++ code/make_audio_labels.py
@@ -6,24 +6,24 @@ the Mac OS text-to-speech synthesizer, so must be run on a Mac.
 import subprocess
 
 
-def say(utterance, output_path):
-	subprocess.check_output(['say', f'"{utterance}"', '-v', 'Tessa', '-o', output_path])
+def say(utterance, voice, output_path):
+	subprocess.check_output(['say', f'"{utterance}"', '-v', voice, '-o', output_path])
 
 
 # Audio check sentences
 
 for i, sentence in enumerate(['the quick cat', 'the green bird', 'the hungry dog', 'the angry lion']):
 	path = f'../experiment/client/sounds/test{i}.m4a'
-	say(sentence, path)
+	say(sentence, 'Tessa', path)
 
-say('This is an attention check; please click on the object.', '../experiment/client/sounds/catch_instruction.m4a')
-say('Great. Please continue.', '../experiment/client/sounds/catch_acknowledge.m4a')
+say('This is an attention check; please click on the object.', 'Tessa', '../experiment/client/sounds/catch_instruction.m4a')
+say('Great. Please continue.', 'Tessa', '../experiment/client/sounds/catch_acknowledge.m4a')
 
 
 # Experiment 1 spoken word stimuli
 
 for i, word in enumerate(['booveekoe', 'zeteekoe', 'gafeekoe', 'woppeekoe']):
-	say(word, f'../experiment/client/words/{i}.m4a')
+	say(word, 'Tessa', f'../experiment/client/words/{i}.m4a')
 
 
 # Experiment 2 spoken word stimuli
@@ -32,4 +32,16 @@ for i, stem in enumerate(['boovy', 'zetty', 'gaffy', 'woppy']):
 	for c, cons in enumerate(['th', 's', 'sh', 'h']):
 		for v, vowl in enumerate(['a', 'ay', 'oe', 'u']):
 			word = f'{stem}{cons}{vowl}'
-			say(word, f'../experiment/client/words/{i}_{c}_{v}.m4a')
+			say(word, 'Tessa', f'../experiment/client/words/{i}_{c}_{v}.m4a')
+
+
+# Experiment 3 spoken word stimuli
+
+for i, word in enumerate(['boovikoe', 'zettikoe', 'woppikoe']):
+	say(word, 'Tessa', f'../experiment/client/words/{i}.m4a')
+
+for i, stem in enumerate(['boovi', 'zetti', 'woppi']):
+	for c, cons in enumerate(['f', 's', 'sh']):
+		for v, vowl in enumerate(['oe', 'a', 'ay']):
+			word = f'{stem}{cons}{vowl}'
+			say(word, 'Moira', f'../experiment/client/words/{i}_{c}_{v}.m4a')
