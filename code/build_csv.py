41ead83ef9e54e078ee3b6f79025f8d64ec041ea
Jon Carr <4412666+jwcarr@users.noreply.github.com>
2024-01-19 14:25:10 +0000

    Include structure score in main csv

--- code/build_csv.py
+++ code/build_csv.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import Levenshtein
 from utils import json_load
+import mantel
 
 
 ROOT = Path(__file__).parent.parent.resolve()
@@ -31,6 +32,22 @@ def communicative_cost(lexicon, dims):
 	U = product(*[range(n_values) for n_values in dims])
 	U_size = np.product(dims)
 	return 1 / U_size * sum([-np.log2(1 / len(reverse_lexicon[lexicon[m]])) for m in U])
+
+def structure(lexicon, dims):
+	strings = []
+	meanings = []
+	for meaning, signal in lexicon.items():
+		strings.append(signal)
+		meanings.append(meaning)
+	string_dists = []
+	for i in range(len(strings)):
+		for j in range(i+1, len(strings)):
+			string_dists.append(Levenshtein.distance(strings[i], strings[j]))
+	meaning_dists = []
+	for i in range(len(meanings)):
+		for j in range(i+1, len(meanings)):
+			meaning_dists.append(Levenshtein.distance(meanings[i], meanings[j]))
+	return mantel.test(string_dists, meaning_dists).z
 
 def communicative_success(subject_a_id, subject_b_id):
 	subject_a = json_load(ROOT / 'data' / 'exp' / f'subject_{subject_a_id}.json')
@@ -72,7 +89,7 @@ def build_csv(exp_data_file, exp_csv_file):
 				lexicon_a = convert_lexicon_meanings_to_tuple(subject_a['lexicon'])
 				error = transmission_error(lexicon_a, prev_lexicon) if prev_lexicon else None
 				cost = communicative_cost(lexicon_a, (3, 3))
-				struc = 0
+				struc = structure(lexicon_a, (3, 3))
 				if subject_b:
 					lexicon_b = convert_lexicon_meanings_to_tuple(subject_b['lexicon'])
 					success = communicative_success(subject_a['subject_id'], subject_b['subject_id'])
@@ -86,9 +103,10 @@ def build_csv(exp_data_file, exp_csv_file):
 					cost,
 					error,
 					success,
+					struc,
 				])
 				prev_lexicon = lexicon_a
-	df = pd.DataFrame(table, columns=['condition', 'chain', 'generation', 'epoch', 'cost', 'error', 'comm_success'])
+	df = pd.DataFrame(table, columns=['condition', 'chain', 'generation', 'epoch', 'cost', 'error', 'comm_success', 'structure'])
 	df.to_csv(exp_csv_file, index=False)
 
 
